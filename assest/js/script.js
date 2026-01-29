5a88ee0065b5f4f83ef6e1b990215411e98c5bdf
yahya <y40y50@gmail.com>
2026-01-29 18:23:58 +0000

    display addition questions to the user

--- assest/js/script.js
+++ assest/js/script.js
@@ -26,6 +26,12 @@ function runGame(gameType) {
     } 
 }
 function checkAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        
 
 }
 function calculateCorrectAnswer() {
